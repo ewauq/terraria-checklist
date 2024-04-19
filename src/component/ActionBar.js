8bdcfad96e2a665306c1ec1527c9b4f8d3527aad
ewauq <emilien.wauquier@gmail.com>
2024-04-19 11:06:30 +0200

    Remove useless comment

--- src/component/ActionBar.js
+++ src/component/ActionBar.js
@@ -1,6 +1,5 @@
 export default class ActionBar {
   constructor() {
-    // this.actionBarNode = document.querySelector('div.action-bar')
     this.drawerToggleButton = document.querySelector('button.drawer-toggle')
     this.drawerNode = document.querySelector('div.drawer')
   }
