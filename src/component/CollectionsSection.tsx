a5c164eac91afef7482415423a76a14e20db6c2b
ewauq <emilien.wauquier@gmail.com>
2024-05-24 13:15:59 +0200

    Update the collection done items count

--- src/component/CollectionsSection.tsx
+++ src/component/CollectionsSection.tsx
@@ -17,19 +17,32 @@ const Collections = ({
     onPageSelected({ type: 'collection', content: collection })
   }
 
+  const getDoneItemsCount = (chapterId: number): number => {
+    const collectionItems = Object.keys(localStorage).filter((key) =>
+      key.startsWith(`collection-${chapterId}`),
+    )
+    const doneItems = collectionItems.filter((key) => localStorage.getItem(key) === 'true')
+    return doneItems.length
+  }
+
   return (
     <section>
       <h3>{title}</h3>
       <div className="chapters">
         {collections.map((collection) => {
+          const doneItemsCount = getDoneItemsCount(collection.id)
           const itemsCount = collection.items.filter((item) => typeof item === 'object').length
           return (
             <a
               key={collection.id}
               href={`#${collection.slug}`}
               onClick={() => handleCollectionClick(collection)}
+              className={doneItemsCount === itemsCount ? 'done' : ''}
             >
-              {collection.label} <span className="done-count">(00/{itemsCount})</span>
+              {collection.label}{' '}
+              <span className="done-count">
+                ({doneItemsCount}/{itemsCount})
+              </span>
             </a>
           )
         })}
