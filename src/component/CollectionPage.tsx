c8382c0c4e307e85c8fd865612442df1285864f2
ewauq <emilien.wauquier@gmail.com>
2024-05-28 00:13:16 +0200

    Code cleaning

--- src/component/CollectionPage.tsx
+++ src/component/CollectionPage.tsx
@@ -4,10 +4,9 @@ import Item from './Item'
 
 interface ColletionPageProps {
   page: Collection | null | undefined
-  onItemChecked: (localStorageKeyValue: string) => void
 }
 
-const CollectionPage = ({ page, onItemChecked }: ColletionPageProps): JSX.Element => {
+const CollectionPage = ({ page }: ColletionPageProps): JSX.Element => {
   return (
     <div className="page">
       <div className="cover" style={{ background: page?.background }}>
@@ -17,10 +16,7 @@ const CollectionPage = ({ page, onItemChecked }: ColletionPageProps): JSX.Elemen
       <div className="content collection">
         {page?.items.map((item, index) => {
           if (typeof item === 'string') return <h3 key={index}>{item}</h3>
-
-          return (
-            <Item key={index} item={item} collectionId={page.id} onItemChecked={onItemChecked} />
-          )
+          return <Item key={index} item={item} collectionId={page.id} />
         })}
       </div>
     </div>
