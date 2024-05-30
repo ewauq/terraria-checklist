f244a3ade42cb70eb1f517157fdd2a1ea85f33bd
ewauq <emilien.wauquier@gmail.com>
2024-05-30 19:00:17 +0200

    Update the webpack asset config

--- webpack.config.js
+++ webpack.config.js
@@ -41,15 +41,7 @@ module.exports = {
       __BUILD_DATE__: JSON.stringify(new Date().toISOString().slice(0, 10)),
     }),
     new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/image/armor', to: 'image/armor' },
-        { from: 'src/image/artwork', to: 'image/artwork' },
-        { from: 'src/image/entity', to: 'image/entity' },
-        { from: 'src/image/icon', to: 'image/icon' },
-        { from: 'src/image/logo', to: 'image/logo' },
-        { from: 'src/image/relic', to: 'image/relic' },
-        { from: 'src/image/trophy', to: 'image/trophy' },
-      ],
+      patterns: [{ from: 'src/image', to: 'image' }],
     }),
   ],
   output: {
